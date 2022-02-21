bcd80a727725d460706fef4e1016efc10a0a40f5
sanpedro ezequiel <sanpedrofabianezequiel@gmail.com>
2022-02-21 15:27:55 -0300

    Create componente router and deploy to GCLOUD

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import {json} from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(json());
 
 
-app.get('/api/users/currentuser',(req,res)=>{
-    res.send("Hello");
-});
+app.use(currentUserRouter);
+
+
 
 app.listen(3000,()=>{
     console.log('Listening 3000')
---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/api/users/currentuser',(req,res)=>{
+
+})
+export {
+    router as currentUserRouter
+}
